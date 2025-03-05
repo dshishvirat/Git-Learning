88c791cb4ee4d2801e19c953e72ea2e2b0fedf55
Dev Ashish <dshish719@gmail.com>
2025-03-05 16:37:00 +0530

    My seven commit

---
+++ Day01/first.js
@@ -0,0 +1,22 @@
+const name = "Deva";
+const age = 20;
+
+let bal = 520;
+
+function updateba(value)
+{
+    bal+=value;
+}
+
+console.log("hello deva")
+
+console.log("Hello Deva")
+
+const c = 10+20;
+console.log(c);
+
+function balance()
+{
+    console.log("update")
+}
+
