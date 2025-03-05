7193a0202c88743d545587b8286538586856d096
Dev Ashish <dshish719@gmail.com>
2025-03-05 18:34:48 +0530

    six commit

--- Day02/first.js
+++ Day02/first.js
@@ -2,13 +2,6 @@ const name = "Deva"
 const age = 20;
 const balanace = 345;
 
+amount = 345;
 
-
-function updatebalance(){
-    balanace+=23;
-}
-
-updatebalance()
-{
-    console.log("hello bhai")
-}
+ console.log("hello bhai")
